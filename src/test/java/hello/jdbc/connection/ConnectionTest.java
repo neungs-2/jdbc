244734443e51f4c4b05399446425d7517e038d52
logan <gnlee95@blocko.io>
2023-12-28 18:10:29 +0900

    Connection Pool

--- src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -1,5 +1,6 @@
 package hello.jdbc.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -31,10 +32,32 @@ public class ConnectionTest {
         useDataSource(dataSource);
     }
 
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        //커넥션 풀링: HikariProxyConnection(Proxy) -> JdbcConnection(Target)
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+        useDataSource(dataSource);
+        Thread.sleep(1000); //커넥션 풀에서 커넥션 생성 시간 대기
+    }
+
     private void useDataSource(DataSource datasource) throws SQLException {
         Connection con1 = datasource.getConnection();
         Connection con2 = datasource.getConnection();
-        log.info("connection={}, class={}", con1, con1.getClass());
-        log.info("connection={}, class={}", con2, con2.getClass());
+//        Connection con3 = datasource.getConnection();
+//        Connection con4 = datasource.getConnection();
+//        Connection con5 = datasource.getConnection();
+//        Connection con6 = datasource.getConnection();
+//        Connection con7 = datasource.getConnection();
+//        Connection con8 = datasource.getConnection();
+//        Connection con9 = datasource.getConnection();
+//        Connection con10 = datasource.getConnection();
+//        Connection con11 = datasource.getConnection();
+        log.info("\nconnection={}, \nclass={}", con1, con1.getClass());
+        log.info("\nconnection={}, \nclass={}", con2, con2.getClass());
     }
 }
