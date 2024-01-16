eb1e44ae25a2149db06ffa68d71945e91ae9d8e3
logan <gnlee95@blocko.io>
2024-01-17 04:25:28 +0900

    @Transactional AOP 적용

--- src/main/java/hello/jdbc/service/MemberServiceV3_3.java
+++ src/main/java/hello/jdbc/service/MemberServiceV3_3.java
@@ -4,25 +4,24 @@ import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepositoryV3;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.TransactionTemplate;
 import java.sql.SQLException;
 
 /**
- * 트랜잭션 - 트랜잭션 AOP
+ * 트랜잭션 - @Transactional AOP
  */
 @Slf4j
 public class MemberServiceV3_3 {
     private final MemberRepositoryV3 memberRepository;
-    private final TransactionTemplate txTemplate;
 
     public MemberServiceV3_3(
-            MemberRepositoryV3 memberRepository,
-            PlatformTransactionManager transactionManager
+            MemberRepositoryV3 memberRepository
     ) {
         this.memberRepository = memberRepository;
-        this.txTemplate = new TransactionTemplate(transactionManager);
     }
 
+    @Transactional
     public void accountTransfer(String fromId, String toId, int money) throws SQLException {
         bizLogic(fromId, toId, money);
     }
